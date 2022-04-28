7b6dd8d20702d74d75c969adde8029a63d65b9e0
Alfin Joseph <95361348+AlfinJ0se@users.noreply.github.com>
2022-04-28 18:13:15 +0530

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -1,11 +1,16 @@
 <?php
+if(isset($_GET['inp']))
+        {
         $input = $_GET['inp'];
         $black = array("ls","pwd","cat","ping","man"," ","&","|",);
         $result = str_replace($black,"",$input);
         $op = shell_exec("man ".$result);
         echo $result;
-                if(isset($_GET['src'])){
-                        highlight_file('index.php');
-                }
-        echo '<pre>'.$op.'</pre>';
+        echo "<pre>".$op."</pre>";
+}
+
+if(isset($_GET['src'])){
+        highlight_file('index.php');
+}
 ?>
+
